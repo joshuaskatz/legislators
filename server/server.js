8396094bec0e872e11bd63b6126c2ca3bb77614d
Joshua <fujin95@gmail.com>
2020-07-05 11:24:42 -0400

    server port update

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res, next) => {
+app.get('/', (req, res, next) => {
 	res.sendFile(path.join(publicPath, 'index.html'));
 });
 
